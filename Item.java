f7df7c2ca4b37211e2ccd563ae835f20d2cbd728
moerukouri <twebblemc@gmail.com>
2026-08-16 20:49:47 +0800

    Added item class getters and setters, table formatting functions

--- Item.java
+++ Item.java
@@ -12,4 +12,37 @@ public class Item{
         this.quantity = quantity;
         this.price = price;
     }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toTableRow(int categoryWidth, int idWidth, int nameWidth, int quantityWidth, int priceWidth) {
+        String format = " %-" + categoryWidth + "s | %-" + idWidth + "s | %-" + nameWidth + "s | %-" + quantityWidth + "d | %-" + priceWidth + ".2f";
+        return String.format(format, category, id, name, quantity, price);
+    }
 }
